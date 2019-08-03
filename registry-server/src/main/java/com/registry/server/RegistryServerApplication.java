b776e504537ec730cad2e70f8f9d2958253cecfa
serhad.burakan <serhad.burakan@accenture.com>
2019-08-03 18:32:02 +0300

    initial commit

---
+++ registry-server/src/main/java/com/registry/server/RegistryServerApplication.java
@@ -0,0 +1,14 @@
+package com.registry.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class RegistryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryServerApplication.class, args);
+	}
+
+}
