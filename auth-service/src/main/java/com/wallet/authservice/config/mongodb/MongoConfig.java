301e38930b3cba9d46355970bc4962e6b7c9aa49
serhad.burakan <serhad.burakan@accenture.com>
2019-08-06 14:30:38 +0300

    mongodb

--- auth-service/src/main/java/com/wallet/authservice/config/mongodb/MongoConfig.java
+++ auth-service/src/main/java/com/wallet/authservice/config/mongodb/MongoConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.marcusdacoregio.authservice.repository")
+@EnableMongoRepositories(basePackages = "com.wallet.authservice.repository")
 public class MongoConfig {
 
 }
--- auth-service/src/main/java/com/wallet/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/wallet/authservice/repository/UserRepository.java
@@ -1,14 +1,15 @@
 package com.wallet.authservice.repository;
 
-import com.wallet.authservice.domain.User;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.wallet.authservice.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
-    Optional<User> findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
 }
