76414cce93b66d0255915521896e7df99dc82dfd
FXMETER PROJECt <proje.fxmeter@gmail.com>
2019-08-16 10:44:48 +0300

    d

---
+++ docker.bat
@@ -0,0 +1,29 @@
+call cd mysql-server
+call docker build -t my-mysql .
+call docker run -d -p 3306:3306 --name my-mysql -e MYSQL_ROOT_PASSWORD=root my-mysql
+call cd ..
+call cd config-server
+call mvnw install
+call docker build -t config-server .
+call docker run -d -p 8888:8888 config-server:latest
+call cd ..
+call cd eureka-server
+call mvnw install
+call docker build -t eureka-server .
+call docker run -d -p 9091:9091 eureka-server:latest
+call cd ..
+call cd auth-server
+call mvnw install
+call docker build -t auth-server .
+call docker run -d -p 9999:9999 auth-server:latest
+call cd ..
+call cd gateway-service
+call mvnw install
+call docker build -t gateway-service .
+call docker run -d -p 9092:9092 gateway-service:latest
+call cd ..
+call cd wallet-service
+call mvnw install -DskipTests
+call docker build -t wallet-service .
+call docker run -d -p 9093:9093 wallet-service:latest
+call cd ..
---
+++ wallet-service/src/test/java/com/wallet/ApplicationTests.java
@@ -0,0 +1,139 @@
+package com.wallet;
+
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import javax.security.auth.login.AccountNotFoundException;
+import javax.validation.Validator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.wallet.auth.service.CustomUserDetailsService;
+import com.wallet.dto.Deposit;
+import com.wallet.dto.Transaction;
+import com.wallet.entity.AccountEntity;
+import com.wallet.entity.TransactionHistoryEntity;
+import com.wallet.entity.User;
+import com.wallet.exception.AccountBalanceCannotBeLessThanZeroException;
+import com.wallet.exception.NoUserFoundException;
+import com.wallet.exception.TransactionNumberAlreadyExists;
+import com.wallet.repository.AccountEntityRepository;
+import com.wallet.repository.TransactionHistoryEntityRepository;
+import com.wallet.repository.UserRepository;
+import com.wallet.service.AccountService;
+import com.wallet.service.TransactionHistoryService;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class ApplicationTests {
+
+	@Mock
+	private AccountEntityRepository accountEntityRepository;
+
+	@Mock
+	private TransactionHistoryEntityRepository transactionHistoryEntityRepository;
+
+	@Mock
+	private Validator validator;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@InjectMocks
+	private CustomUserDetailsService userDetailsService;
+
+	@InjectMocks
+	private TransactionHistoryService transactionHistoryService;
+
+	private AccountService accountService;
+
+	String username = "test";
+	BigDecimal amount = new BigDecimal(10);
+	String transactionId = "1";
+	User user;
+	private Deposit deposit;
+
+	@Before
+	public void setValues() {
+		user = createUser();
+
+		when(userRepository.findByEmail(username)).thenReturn(Optional.of(user));
+		when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+		deposit = createDeposit();
+
+		AccountEntity account = createAccount(user);
+		when(accountEntityRepository.findByUser(Matchers.any(User.class))).thenReturn(Optional.of(account));
+		accountService = new AccountService(accountEntityRepository, transactionHistoryService, validator,
+				userDetailsService);
+	}
+
+	@Test
+	public void spendMoneyFromAccountSuccessfully()
+			throws AccountNotFoundException, NoUserFoundException, TransactionNumberAlreadyExists,
+			AccountBalanceCannotBeLessThanZeroException, InterruptedException, ExecutionException {
+
+		Deposit updatedDeposit = accountService.spendMoneyFromAccount(username, amount, transactionId).get();
+		Assert.assertEquals(updatedDeposit.getRemaining().compareTo(BigDecimal.ZERO), 0);
+	}
+
+	@Test(expected = AccountBalanceCannotBeLessThanZeroException.class)
+	public void spendMoneyFromAccountAccountLessThanZero()
+			throws AccountNotFoundException, NoUserFoundException, TransactionNumberAlreadyExists,
+			AccountBalanceCannotBeLessThanZeroException, InterruptedException, ExecutionException {
+
+		accountService.spendMoneyFromAccount(username, new BigDecimal(15), transactionId).get();
+	}
+
+	@Test
+	public void addMoneyToAccountSuccessfully()
+			throws AccountNotFoundException, NoUserFoundException, TransactionNumberAlreadyExists,
+			AccountBalanceCannotBeLessThanZeroException, InterruptedException, ExecutionException {
+
+		Deposit updatedDeposit = accountService.addMoneyToAccount(username, amount, transactionId).get();
+		Assert.assertEquals(updatedDeposit.getRemaining().compareTo(new BigDecimal(20)), 0);
+	}
+
+	
+	public User createUser() {
+		User user = new User();
+		user.setId(1L);
+		user.setEmail("test@test.com");
+		user.setEnabled(true);
+		user.setAccountNonExpired(false);
+		user.setAccountNonLocked(false);
+		user.setCredentialsNonExpired(false);
+		return user;
+
+	}
+
+	public AccountEntity createAccount(User user) {
+		AccountEntity account = new AccountEntity();
+		account.setId(5L);
+		account.setUser(user);
+		account.setRemaining(amount);
+		return account;
+	}
+
+	public Deposit createDeposit() {
+		Deposit deposit = new Deposit();
+		deposit.setCredit(amount);
+		deposit.setTransactionId(transactionId);
+		deposit.setRemaining(amount);
+		deposit.setUsername(username);
+		return deposit;
+	}
+
+	
+
+}
